9793f44f08433a434f0a7aff98b1b01e9ab18244
Guido Accardo <gaccardo@gmail.com>
2014-01-17 10:26:18 -0300

    Version and documentation changes

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 #from distutils.core import setup
 from setuptools import setup
+import textwrap
 
 setup(
     name='Pybles',
-    version='1.0.8',
+    version='1.0.10',
     author='Guido Accardo',
     author_email='gaccardo@gmail.com',
     packages=['pybles'],
     url='http://pypi.python.org/pypi/Pybles/',
-    license='LICENSE.txt',
+    license='GPLv2',
     description='Module to represent tables in the terminal using python',
     long_description=open('README.txt').read(),
+    keywords="ssh networking console tools",
+    classifiers = textwrap.dedent("""
+        Operating System :: OS Independent
+        Programming Language :: Python :: 2.7
+        Topic :: System :: Systems Administration
+        Topic :: System :: Networking
+        Topic :: Utilities
+        Topic :: Terminals
+        Environment :: Console
+        License :: OSI Approved :: GNU General Public License (GPL)
+        """).strip().splitlines(),
     install_requires=[
         "blessings >= 1.5.1",
     ],)
